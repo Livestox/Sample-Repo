64b1e928425114a88f27abb8a2d840305dfa4d6b
Livestox <stoian.tonev@gmial.com>
2021-01-03 11:41:28 +0200

    Removed

--- script.py
+++ script.py
@@ -1,16 +1,5 @@
-import math
-import sys
-from os import rename
-
 import requests
 
-
-print(sys.api_version)
-# print(sys.executable)
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
-
 r = requests.get("http://localhost/poultry/api/partners.php")
-# print(r.text)
+print(r.ok)
+print(r.text)
